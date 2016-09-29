1f529766ad842010bfd5d1b71ac0a5255737f50c
edgamat <edgamat@outlook.com>
2016-09-29 06:09:48 -0400

    Tests with dual select

--- src/skeleton/src/roles/role-home.js
+++ src/skeleton/src/roles/role-home.js
@@ -4,6 +4,10 @@ export class RoleHome {
 
     selectedRoleId = 1;
 
+    assignedPermissionList = [];
+
+    unassignedPermissionList = [];
+
     roles = 
     [
         { roleId: 1, roleName: "Admin" },
@@ -16,6 +20,77 @@ export class RoleHome {
         { userId: 1, userName: "DOEJ", fullName: "John Doe" },
         { userId: 2, userName:"SECORDC", fullName: "Cliff Secord" }
     ];
+
+    assignedPermissions = 
+    [
+        { permissionId: 1, permissionName: "ACCOUNT VIEW" },
+        { permissionId: 2, permissionName: "ACCOUNT UPDATE" }
+    ]
+
+    unassignedPermissions = 
+    [
+        { permissionId: 3, permissionName: "ACCOUNT CREATE" },
+        { permissionId: 4, permissionName: "ACCOUNT DELETE" }
+    ]
+
+    movePermission(e, action) {
+        console.log(action);
+
+        if (action === "removeAll") {
+            for (var i = 0; i < this.unassignedPermissions.length; i++) {
+                this.assignedPermissions.push(this.unassignedPermissions[i]);
+            }
+
+            this.unassignedPermissions = [];
+        }
+
+        if (action === "addAll") {
+            for (var i = 0; i < this.assignedPermissions.length; i++) {
+                this.unassignedPermissions.push(this.assignedPermissions[i]);
+            }
+
+            this.assignedPermissions = [];
+        }
+
+        if (action === "add") {
+
+            let itemsToRemove = [];
+
+            for (var i = 0; i < this.assignedPermissionList.length; i++) {
+                
+                console.log(this.assignedPermissionList[i]);
+
+                let itemToRemove = this.assignedPermissions.indexOf(this.assignedPermissionList[i]);
+
+                //// this.unassignedPermissions.push(this.assignedPermissions[itemToRemove]);
+
+                //// itemsToRemove.push(this.assignedPermissions.indexOf(this.assignedPermissions[i]));
+
+                console.log(itemToRemove);
+
+                //// works: this.assignedPermissions.splice(itemsToRemove[i], 1);
+                //// this.assignedPermissions.splice(itemsToRemove[i], 1);
+            }
+
+            for (var i = 0; i < itemsToRemove.length; i++) {
+                //// this.assignedPermissions.splice(itemsToRemove[i], 1);
+            }
+        }
+
+        this.assignedPermissionList.forEach(function (li) {
+            console.log(li);
+        });
+
+
+        for (var i = 0; i < this.assignedPermissionList.length; i++) {
+            console.log(this.assignedPermissionList[i]);
+        }
+
+        for (var i = 0; i < unassignedPermissionList.length; i++) {
+            ////console.log(unassignedPermissionList[i].selected);
+        }
+
+    }
 
     selectedRoleChange(e) {
         console.log(e);
